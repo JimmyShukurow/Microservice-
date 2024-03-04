eff8e072e5610009219c94d0979ba5d596eda90a
Your Name <you@example.com>
2024-03-04 19:01:17 +0300

    add article method tests are ready

--- smartir.website/src/test/java/io/smartir/smartir/website/service/ArticleServiceTest.java
+++ smartir.website/src/test/java/io/smartir/smartir/website/service/ArticleServiceTest.java
@@ -1,56 +1,101 @@
 package io.smartir.smartir.website.service;
 
 import io.smartir.smartir.website.HelperFunctions;
+import io.smartir.smartir.website.entity.Article;
+import io.smartir.smartir.website.exceptions.ArticleWithThisTitleExistsException;
 import io.smartir.smartir.website.model.ArticleContentsModel;
 import io.smartir.smartir.website.repository.ArticleRepository;
+import io.smartir.smartir.website.repository.TagRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.nio.file.Path;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.Mockito.*;
 
 
-
+@ExtendWith(MockitoExtension.class)
 class ArticleServiceTest extends HelperFunctions {
 
 
-//    @Autowired
-//    public TypeRepository typeRepository;
-//
-//    @Autowired
-//    public TagRepository tagRepository;
+    @Mock
+    private FileService fileService;
 
-    @Autowired
-    public  ArticleRepository articleRepository;
+    @Mock
+    private TagRepository tagRepository;
 
-    @MockBean
-    @Autowired
+    @Mock
+    private   ArticleRepository articleRepository;
+
+
     public  ArticleService articleService;
 
 
+    @BeforeEach
+    void setUp() {
+        articleService = new ArticleService(fileService, articleRepository, tagRepository);
+    }
+
     @Test
+    @DisplayName("Add article success option ")
     public void addArticle() throws Exception {
         //given
-//        var type = typeRepository.save(typeCreator());
-//        var tag = tagRepository.save(tagCreator(type));
-//        var imageFile = createTestImage();
-//        var bannerImage = createTestImage();
-//        System.out.println("image is =>" + imageFile);
-//        var title = "test";
-//        ArticleContentsModel model = new ArticleContentsModel("test", "test");
-//        var summary = "test summary";
-//        articleService.addArticle(imageFile, title, bannerImage, List.of(model), List.of(1), summary);
-//        //when
-//        var expected = articleRepository.findAll();
-//        //then
-//        assertThat(expected.size()).isEqualTo(0);
+        var imageFile = createTestImage();
+        var bannerImage = createTestImage();
+        var title = "test";
+        ArticleContentsModel model = new ArticleContentsModel("test", "test");
+        var summary = "test summary";
+
+        //when
+        when(articleRepository.findByTitleContainsAllIgnoreCase(title)).thenReturn(Optional.empty());
+        when(fileService.moveImgFile(imageFile)).thenReturn(Path.of("image/path"));
+        when(fileService.moveBannerImageFile(bannerImage)).thenReturn(Path.of("bannerImage/path"));
+        when(articleRepository.save(Mockito.any(Article.class))).thenAnswer(invocationOnMock -> invocationOnMock.getArgument(0));
+        var expected = articleService.addArticle(imageFile, title, bannerImage, List.of(model), List.of(), summary);
+
+        //then
+        assertThat(expected).isEqualTo("Article added");
+        verify(articleRepository, times(1)).save(Mockito.any(Article.class));
+
+    }
+
+    @Test
+    @DisplayName("Add article throws exception and not saving artice")
+    public void addArticleThrowException() throws Exception {
+        //given
+        var imageFile = createTestImage();
+        var bannerImage = createTestImage();
+        var title = "test";
+        ArticleContentsModel model = new ArticleContentsModel("test", "test");
+        var summary = "test summary";
+
+        //when
+        when(articleRepository.findByTitleContainsAllIgnoreCase(title)).thenReturn(Optional.of(new Article()));
+
+        //then
+        assertThatThrownBy(() -> {
+            articleService.addArticle(imageFile, title, bannerImage, List.of(model), List.of(), summary);
+
+        }).isInstanceOf(ArticleWithThisTitleExistsException.class);
+        verify(articleRepository, never()).save(Mockito.any(Article.class));
+
     }
 
     @Test
     public void updateArticle() {
+
+        //given
+        //when
+        //then
     }
 
     @Test
@@ -68,4 +113,6 @@ class ArticleServiceTest extends HelperFunctions {
     @Test
     public void filterBySearch() {
     }
+
+
 }
