93e1d8cff5b14a3ebe7549be0c144d60ce04227b
onur <onuripekcii@gmail.com>
2024-03-25 11:18:07 +0300

    feedback update

--- smartir.website/src/main/java/io/smartir/smartir/website/controller/FeedbackController.java
+++ smartir.website/src/main/java/io/smartir/smartir/website/controller/FeedbackController.java
@@ -1,10 +1,7 @@
 package io.smartir.smartir.website.controller;
 
-import io.smartir.smartir.website.entity.Type;
 import io.smartir.smartir.website.requests.FeedbackRequest;
 import io.smartir.smartir.website.service.FeedbackService;
-import io.smartir.smartir.website.service.TypeService;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
